92797d139c3eaa62725549d662260b7519d3cfdc
Tanishamanickavelan <tanisha.manick04@gmail.com>
2025-09-26 00:55:23 +0530

    ran 01_.py file

--- rename_vdos.py
+++ rename_vdos.py
@@ -1,7 +1,7 @@
 import os
 
 # Path to your shoplifting videos folder
-folder = r"D:\sentinel_eye\videos\shoplifting"
+folder = r"D:\sentinel_eye\videos\normal"
 
 # List all mp4 files
 files = [f for f in os.listdir(folder) if f.endswith(".mp4")]
@@ -9,7 +9,7 @@ files.sort()  # Optional: sort alphabetically
 
 # Rename files sequentially
 for i, f in enumerate(files, start=1):
-    new_name = f"shoplifting_{i:02d}.mp4"
+    new_name = f"normal_{i:02d}.mp4"
     os.rename(os.path.join(folder, f), os.path.join(folder, new_name))
 
 print("Renaming completed!")
